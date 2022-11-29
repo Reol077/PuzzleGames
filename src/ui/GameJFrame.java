93c6bd2f02baafc8773a54d754a6489e4a38c99e
FreeReol <2571219329@qq.com>
2022-11-29 20:25:17 +0800

    初始化菜单

--- src/ui/GameJFrame.java
+++ src/ui/GameJFrame.java
@@ -6,6 +6,41 @@ public class GameJFrame extends JFrame {
     //游戏主界面
 
     public GameJFrame() {
+        initJFrame();
+
+        initJMenuBar();
+        //显示窗口
+        this.setVisible(true);
+
+    }
+
+    private void initJMenuBar() {
+        //初始化菜单
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu functionJMenu = new JMenu("功能");
+        JMenu aboutJMenu = new JMenu("关于我们");
+
+        JMenuItem replayItem = new JMenuItem("重新游戏");
+        JMenuItem reLoginItem = new JMenuItem("重新登录");
+        JMenuItem closeItem = new JMenuItem("关闭游戏");
+
+        JMenuItem accountItem = new JMenuItem("公众号");
+
+        functionJMenu.add(replayItem);
+        functionJMenu.add(reLoginItem);
+        functionJMenu.add(closeItem);
+
+        aboutJMenu.add(accountItem);
+
+        jMenuBar.add(functionJMenu);
+        jMenuBar.add(aboutJMenu);
+
+        //给整个界面设置菜单
+        this.setJMenuBar(jMenuBar);
+    }
+
+    private void initJFrame() {
         //窗口大小
         this.setSize(603, 680);
         //标题
@@ -16,15 +51,6 @@ public class GameJFrame extends JFrame {
         this.setLocationRelativeTo(null);
         //关闭模式
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        //初始化菜单
-        JMenuBar jMenuBar = new JMenuBar();
-
-        JMenu functionJMenu = new JMenu();
-        JMenu aboutJMenu = new JMenu();
-dvvf
-        //显示窗口
-        this.setVisible(true);
     }
 
 }
