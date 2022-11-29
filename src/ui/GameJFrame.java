08c0daa22c0c83af3c4acb381083d31be43b684d
FreeReol <2571219329@qq.com>
2022-11-29 19:02:06 +0800

    init

--- src/ui/GameJFrame.java
+++ src/ui/GameJFrame.java
@@ -5,12 +5,25 @@ import javax.swing.*;
 public class GameJFrame extends JFrame {
     //游戏主界面
 
-    public GameJFrame(){
-        this.setSize(603,680);
+    public GameJFrame() {
+        //窗口大小
+        this.setSize(603, 680);
+        //标题
         this.setTitle("拼图单机版 v1.0");
+        //保持窗口前台
         this.setAlwaysOnTop(true);
+        //页面居中
         this.setLocationRelativeTo(null);
+        //关闭模式
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //初始化菜单
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu functionJMenu = new JMenu();
+        JMenu aboutJMenu = new JMenu();
+dvvf
+        //显示窗口
         this.setVisible(true);
     }
 
--- src/ui/LoginJFrame.java
+++ src/ui/LoginJFrame.java
@@ -7,6 +7,10 @@ public class LoginJFrame extends JFrame {
 
     public LoginJFrame(){
         this.setSize(488,430);
+        this.setTitle("拼图登录界面");
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 }
